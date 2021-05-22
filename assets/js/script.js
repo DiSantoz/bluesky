37972fa3ddd9e4b135e6fc70964565ccc94e65ca
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-22 09:21:24 -0400

    add lat and lon variable for city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
-fetch (
-    'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8f4b5fb79bf55ca4186b297ac79fb394'
-)
+function currentWeather (){
+    // var cityName = document.getElementById = ('#city').value
+    fetch (
+        'https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=8f4b5fb79bf55ca4186b297ac79fb394'
+    )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+
+            var lat = data.coord.lat;
+            console.log(data.coord.lat);
+            var lon = data.coord.lon;
+            console.log(data.coord.lon);
+            
+
+    return fetch (
+        'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=8f4b5fb79bf55ca4186b297ac79fb394'
+    )
     .then(function(response){
         return response.json();
     })
-    .then(function(data){
-        console.log(data);
-    })
+})
+    
+}
+
+currentWeather();
