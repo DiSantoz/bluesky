66953a5b355a0e5f9725f6040fb797a930977048
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-22 11:32:17 -0400

    create two seperate functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,46 @@
 var lat;
+var lat1;
 var lon;
+var lon1;
 
-function currentWeather (){
-    // var cityName = document.getElementById = ('#city').value
-    fetch (
+function currentWeather() {
+    
+    fetch(
         'https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=8f4b5fb79bf55ca4186b297ac79fb394'
     )
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
+            console.log(data);
+            
+            lat = (data.coord.lat);
+            lat1= lat.toString()
+            console.log(lat1);
+
+            lon = data.coord.lon;
+            lon1= lon.toString()
+            console.log(lon1);
+            
+            console.log(coord(lat1, lon1));
+        })
+
+};
+
+function coord(lat1,lon1) {
+
+    fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat1 + '&lon=' + lon1 + '&appid=8f4b5fb79bf55ca4186b297ac79fb394'
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
             console.log(data);
 
-            lat = data.coord.lat;
-            console.log(data.coord.lat);
-            lon = data.coord.lon;
-            console.log(data.coord.lon);
-        });        
-}
+        })
 
-function getCorrd(){ 
-fetch (
-        'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=8f4b5fb79bf55ca4186b297ac79fb394'
-    )
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log("data returned from lat & lon " + data);
-    })
-}
+};
+
 currentWeather();
-getCorrd();
+coord();
