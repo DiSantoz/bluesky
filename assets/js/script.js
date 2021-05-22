f43b5f09eb32a8be1c40823f10ca2b419d47bcd4
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-22 09:56:22 -0400

    test code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var lat;
+var lon;
+
 function currentWeather (){
     // var cityName = document.getElementById = ('#city').value
     fetch (
@@ -9,20 +12,23 @@ function currentWeather (){
         .then(function(data){
             console.log(data);
 
-            var lat = data.coord.lat;
+            lat = data.coord.lat;
             console.log(data.coord.lat);
-            var lon = data.coord.lon;
+            lon = data.coord.lon;
             console.log(data.coord.lon);
-            
+        });        
+}
 
-    return fetch (
-        'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=8f4b5fb79bf55ca4186b297ac79fb394'
+function getCorrd(){ 
+fetch (
+        'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=8f4b5fb79bf55ca4186b297ac79fb394'
     )
     .then(function(response){
         return response.json();
     })
-})
-    
+    .then(function(data){
+        console.log("data returned from lat & lon " + data);
+    })
 }
-
 currentWeather();
+getCorrd();
